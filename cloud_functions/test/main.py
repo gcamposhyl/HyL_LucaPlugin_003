0c1a73813004e80dc54141754a94d7ee24a6f2a5
salazarmelita <asalazar@hyl.cl>
2023-12-13 09:55:31 -0300

    feat: obtain matrice function created

--- cloud_functions/test/main.py
+++ cloud_functions/test/main.py
@@ -1,22 +1,18 @@
 from firebase_functions import https_fn
 
 @https_fn.on_request(max_instances=10)
-def test_orderer_data(req: https_fn.Request) -> https_fn.Response:
-    import pandas as pd
+def plugin_v1_ddjj_obtain_matrice(req: https_fn.Request) -> https_fn.Response:
     import json
-    import os
     from components.utils.data_service import Data_manipulation
 
     try:
-        connection_drive = Data_manipulation()
+        manipulation_data = Data_manipulation()
         if req.method == 'POST':
             request_data = req.get_json()
-            full_data = json.dumps(request_data, indent=2)
-
-            for dicc in request_data:
-                return None
-
+            matrice = manipulation_data.row_orderer(request_data)
+            print("result:", matrice)
+            matrice_json = json.dumps(matrice)
+            return https_fn.Response(matrice_json, status=200, mimetype="application/json")
     except Exception as ex:
         print(str(ex))
-
-    return https_fn.Response("Función iniciada...", status=200, mimetype="application/json")
+        return https_fn.Response("Error al crear matriz", status=500, mimetype="application/json")
